dae3bc83842ed7d42aa50ccdf509adf8861d10dc
wangbinru <wangbinru@sogou-inc.com>
2020-11-26 11:09:22 +0800

    update

---
+++ connection.go
@@ -0,0 +1,51 @@
+package connpool
+
+import (
+	"errors"
+	"net"
+)
+
+type CpConn struct {
+	net.Conn
+	pool *ConnPool
+}
+
+// Destroy will close connection and release connection from connection pool.
+func (conn *CpConn) Destroy() error {
+	if conn.pool == nil {
+		return errors.New("Connection not belong any connection pool.")
+	}
+	err := conn.pool.Remove(conn.Conn)
+	if err != nil {
+		return err
+	}
+	conn.pool = nil
+	return nil
+}
+
+// Close will close the real connection, remove it from the pool, and create a new connection.
+func (conn *CpConn) Close() error {
+	if conn.pool == nil {
+		return errors.New("Connection not belong any connection pool.")
+	}
+
+	if len(conn.pool.conns) < conn.pool.minChannelConnNum {
+		go func() {
+			newConn, err := conn.pool.createConn()
+			if err != nil {
+				return
+			}
+			conn.pool.conns <- newConn
+		}()
+	}
+	err := conn.pool.Remove(conn.Conn) // not sure if this is right
+	return err
+}
+
+// // Close will push connection back to connection pool. It will not close the real connection.
+// func (conn *CpConn) Close() error {
+// 	if conn.pool == nil {
+// 		return errors.New("Connection not belong any connection pool.")
+// 	}
+// 	return conn.pool.Put(conn.Conn)
+// }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/spritesprite/connpool.git
+
+go 1.13
