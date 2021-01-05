ad197d9e95cb4706d2fe9ad811bbcf526f14c441
wangbinru <wangbinru@sogou-inc.com>
2021-01-05 19:46:49 +0800

    update

--- connection.go
+++ connection.go
@@ -1,7 +1,6 @@
 package connpool
 
 import (
-	"errors"
 	"net"
 )
 
@@ -10,36 +9,34 @@ type CpConn struct {
 	pool *ConnPool
 }
 
-// Destroy will close connection and release connection from connection pool.
-func (conn *CpConn) Destroy() error {
-	if conn.pool == nil {
-		return errors.New("Connection not belong any connection pool.")
-	}
-	err := conn.pool.Remove(conn.Conn)
-	if err != nil {
-		return err
-	}
-	conn.pool = nil
-	return nil
-}
+// // Destroy will close connection and release connection from connection pool.
+// func (conn *CpConn) Destroy() error {
+// 	if conn.pool == nil {
+// 		return errors.New("Connection not belong any connection pool.")
+// 	}
+// 	err := conn.pool.Remove(conn.Conn)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	conn.pool = nil
+// 	return nil
+// }
 
 // Close will close the real connection, remove it from the pool, and create a new connection.
 func (conn *CpConn) Close() error {
-	if conn.pool == nil {
-		return errors.New("Connection not belong any connection pool.")
-	}
-
-	go conn.pool.supplementConn()
-
-	return conn.Destroy()
-	// err := conn.pool.Remove(conn.Conn) // not sure if this is right
-	// return err
+	// return conn.Destroy()
+	return conn.Close()
 }
 
-// // Close will push connection back to connection pool. It will not close the real connection.
+// // Close will close the real connection, remove it from the pool, and create a new connection.
 // func (conn *CpConn) Close() error {
 // 	if conn.pool == nil {
 // 		return errors.New("Connection not belong any connection pool.")
 // 	}
-// 	return conn.pool.Put(conn.Conn)
+
+// 	go conn.pool.supplementConn()
+
+// 	return conn.Destroy()
+// 	// err := conn.pool.Remove(conn.Conn) // not sure if this is right
+// 	// return err
 // }
--- pool.go
+++ pool.go
@@ -4,21 +4,26 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math"
 	"net"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
-// ConnPool uses channel buffer connections.
+// ConnPool uses channel to buffer connections.
 type ConnPool struct {
-	lock              sync.Mutex
+	lock              sync.RWMutex // for closed
+	timeLock          sync.RWMutex // for lastSuccTime
 	conns             chan net.Conn
 	minConnNum        int
 	maxConnNum        int
-	totalConnNum      int
+	totalConnNum      int // deprecated
+	minChannelConnNum int // deprecated
 	closed            bool
 	connCreator       func() (net.Conn, error)
-	minChannelConnNum int
+	pendingCount      int32
+	lastSuccTime      float64
 }
 
 var (
@@ -52,6 +57,7 @@ func NewPool(minConn, maxConn, minRemain int, connCreator func() (net.Conn, erro
 	return pool, nil
 }
 
+// NewClosedPool provides an empty closed pool.
 func NewClosedPool() *ConnPool {
 	pool := &ConnPool{}
 	pool.closed = true
@@ -78,6 +84,9 @@ func (p *ConnPool) Get() (net.Conn, error) {
 	}
 
 	go p.supplementConn()
+	if len(p.conns) == 0 {
+		return p.createConn()
+	}
 
 	select {
 	case conn := <-p.conns:
@@ -94,6 +103,9 @@ func (p *ConnPool) GetWithTimeout(timeout time.Duration) (net.Conn, error) {
 	}
 
 	go p.supplementConn()
+	if len(p.conns) == 0 {
+		return p.createConn()
+	}
 
 	select {
 	case conn := <-p.conns:
@@ -109,6 +121,10 @@ func (p *ConnPool) GetWithContext(ctx context.Context) (net.Conn, error) {
 	}
 
 	go p.supplementConn()
+
+	if len(p.conns) == 0 {
+		return p.createConn()
+	}
 
 	select {
 	case conn := <-p.conns:
@@ -127,73 +143,43 @@ func (p *ConnPool) Close() error {
 	p.lock.Lock()
 	defer p.lock.Unlock()
 	p.closed = true
-	close(p.conns)
+	// close(p.conns) // panic is possible if we just close it here
 	for conn := range p.conns {
 		conn.Close()
 	}
 	return nil
 }
 
-// Put can put connection back in connection pool. If connection has been closed, the conneciton will be close too.
-func (p *ConnPool) Put(conn net.Conn) error {
-	if p.isClosed() == true {
-		return errPoolIsClose
-	}
-	if conn == nil {
-		p.lock.Lock()
-		p.totalConnNum = p.totalConnNum - 1
-		p.lock.Unlock()
-		return errors.New("Cannot put nil to connection pool.")
-	}
-
-	select {
-	case p.conns <- conn:
-		// fmt.Printf("[ConnPool] Put() enqueue %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
-		return nil
-	default:
-		return conn.Close()
-	}
-}
-
 func (p *ConnPool) isClosed() bool {
-	p.lock.Lock()
-	ret := p.closed
-	p.lock.Unlock()
-	return ret
+	p.lock.RLock()
+	defer p.lock.RUnlock()
+	return p.closed
 }
 
-// RemoveConn let connection not belong connection pool.And it will close connection.
-func (p *ConnPool) Remove(conn net.Conn) error {
-	if p.isClosed() == true {
-		return errPoolIsClose
-	}
+func (p *ConnPool) setLastSuccTime() {
+	p.timeLock.Lock()
+	defer p.timeLock.Unlock()
+	p.lastSuccTime = GetCurrentTimeInFloat64(3)
+}
 
-	p.lock.Lock()
-	p.totalConnNum = p.totalConnNum - 1
-	p.lock.Unlock()
-	switch conn.(type) {
-	case *CpConn:
-		return conn.(*CpConn).Destroy()
-	default:
-		// fmt.Printf("[ConnPool] Remove() close %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
-		return conn.Close()
-	}
-	return nil
+func (p *ConnPool) getLastSuccTime() float64 {
+	p.timeLock.RLock()
+	defer p.timeLock.RUnlock()
+	return p.lastSuccTime
 }
 
 // createConn will create one connection from connCreator. And increase connection counter.
 func (p *ConnPool) createConn() (net.Conn, error) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-	if p.totalConnNum >= p.maxConnNum {
-		return nil, fmt.Errorf("Connot Create new connection. Now has %d.Max is %d", p.totalConnNum, p.maxConnNum)
-	}
+	// if p.totalConnNum >= p.maxConnNum {
+	// 	return nil, fmt.Errorf("Connot Create new connection. Now has %d.Max is %d", p.totalConnNum, p.maxConnNum)
+	// }
 	conn, err := p.connCreator()
 	if err != nil {
-		return nil, fmt.Errorf("Cannot create new connection.%s", err)
+		return nil, fmt.Errorf("cannot create new connection: %s", err)
 	}
 	// fmt.Printf("[ConnPool] createConn() create %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
-	p.totalConnNum = p.totalConnNum + 1
+	// p.totalConnNum = p.totalConnNum + 1
+	p.setLastSuccTime()
 	return conn, nil
 }
 
@@ -203,24 +189,16 @@ func (p *ConnPool) packConn(conn net.Conn) net.Conn {
 	return ret
 }
 
-func (p *ConnPool) createConnNolock() (net.Conn, error) {
-	if p.totalConnNum >= p.maxConnNum {
-		return nil, fmt.Errorf("Connot Create new connection. Now has %d.Max is %d", p.totalConnNum, p.maxConnNum)
-	}
-	conn, err := p.connCreator()
-	if err != nil {
-		return nil, fmt.Errorf("Cannot create new connection.%s", err)
-	}
-	// fmt.Printf("[ConnPool] createConn() create %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
-	p.totalConnNum = p.totalConnNum + 1
-	return conn, nil
-}
-
 func (p *ConnPool) supplementConn() {
-	p.lock.Lock()
-	defer p.lock.Unlock()
-	if len(p.conns) < p.minChannelConnNum && !p.closed {
-		newConn, err := p.createConnNolock()
+	if p.isClosed() {
+		return
+	}
+
+	atomic.AddInt32(&p.pendingCount, 1)
+	defer atomic.AddInt32(&p.pendingCount, -1)
+
+	if GetCurrentTimeInFloat64(3)-p.getLastSuccTime() < 3 && len(p.conns)+int(atomic.LoadInt32(&p.pendingCount)) < p.maxConnNum {
+		newConn, err := p.createConn()
 		if err != nil {
 			return
 		}
@@ -228,3 +206,65 @@ func (p *ConnPool) supplementConn() {
 		p.conns <- newConn
 	}
 }
+
+// NanoSec .
+const NanoSec float64 = 1000000000
+
+// GetCurrentTimeInFloat64 gets the current time.
+func GetCurrentTimeInFloat64(precision int) float64 {
+	p := math.Pow10(precision)
+	return float64(int(float64(time.Now().UnixNano())/NanoSec*p)) / p
+}
+
+// // Put can put connection back in connection pool. If connection has been closed, the conneciton will be close too.
+// func (p *ConnPool) Put(conn net.Conn) error {
+// 	if p.isClosed() == true {
+// 		return errPoolIsClose
+// 	}
+// 	if conn == nil {
+// 		p.lock.Lock()
+// 		p.totalConnNum = p.totalConnNum - 1
+// 		p.lock.Unlock()
+// 		return errors.New("Cannot put nil to connection pool.")
+// 	}
+
+// 	select {
+// 	case p.conns <- conn:
+// 		// fmt.Printf("[ConnPool] Put() enqueue %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
+// 		return nil
+// 	default:
+// 		return conn.Close()
+// 	}
+// }
+
+// // RemoveConn let connection not belong connection pool.And it will close connection.
+// func (p *ConnPool) Remove(conn net.Conn) error {
+// 	if p.isClosed() == true {
+// 		return errPoolIsClose
+// 	}
+
+// 	p.lock.Lock()
+// 	p.totalConnNum = p.totalConnNum - 1
+// 	p.lock.Unlock()
+// 	switch conn.(type) {
+// 	case *CpConn:
+// 		return conn.(*CpConn).Destroy()
+// 	default:
+// 		// fmt.Printf("[ConnPool] Remove() close %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
+// 		return conn.Close()
+// 	}
+// 	return nil
+// }
+
+// func (p *ConnPool) createConnNolock() (net.Conn, error) {
+// 	// if p.totalConnNum >= p.maxConnNum {
+// 	// 	return nil, fmt.Errorf("Connot Create new connection. Now has %d.Max is %d", p.totalConnNum, p.maxConnNum)
+// 	// }
+// 	conn, err := p.connCreator()
+// 	if err != nil {
+// 		return nil, fmt.Errorf("Cannot create new connection.%s", err)
+// 	}
+// 	// fmt.Printf("[ConnPool] createConn() create %s->%s\n", conn.LocalAddr().String(), conn.RemoteAddr().String())
+// 	// p.totalConnNum = p.totalConnNum + 1
+// 	return conn, nil
+// }
